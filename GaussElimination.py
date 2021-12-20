ffdf2a594c621ccf51dcdbcca86c6245db2bfe56
mariamtarek51 <mariamtarek51@gmail.com>
2021-12-20 16:47:05 +0200

    Update GaussElimination.py

--- GaussElimination.py
+++ GaussElimination.py
@@ -1,5 +1,5 @@
 import numpy as np
-def gauss_pivot(D,g,Aug,numberofunknowns):
+def gauss_pivot(D,g,Aug,numberofunknowns,noofdecimal):
     A=np.array((D),dtype=float)
     f=np.array((g),dtype=float)
     rankA = np.linalg.matrix_rank(A) 
@@ -7,9 +7,9 @@ def gauss_pivot(D,g,Aug,numberofunknowns):
     n = len(f)
     
     if(rankA!=rankAug):
-        print("System is inconsistent and there is no solution")
+        return("System is inconsistent and there is no solution")
     elif(rankA==rankAug<numberofunknowns):
-        print("system has infinite number of solution")
+        return("system has infinite number of solution")
     else:
         for i in range(int(0),int(n-1)):     # Loop through the columns of the matrix
             maxelement=max(A[:,i],key=abs)  #max element in col#i
@@ -22,26 +22,30 @@ def gauss_pivot(D,g,Aug,numberofunknowns):
              #now elimination    
             for j in range(i+1,n):     # Loop through rows below diagonal for each column
                 m = A[j,i]/A[i,i]      #get the multiply
-                roundm=round(m,4)
-                A[j,:] = A[j,:] - roundm*A[i,:]
-                f[j] = f[j] - roundm*f[i] 
+                roundm=round(m,noofdecimal)
+                A[j,:] =A[j,:] - roundm*A[i,:]
+                f[j] = f[j] - roundm*f[i]
            
-        Back_Subs(A,f)
-
-def Back_Subs(A,f):
+        return Back_Subs(A,f,noofdecimal) 
+def Back_Subs(A,f,noofdecimal):
+    
+    
     n = f.size
     x = np.zeros(n)             # Initialize the solution vector, x, to zero
-    x[n-1] = round(f[n-1]/A[n-1,n-1],3)    # Solve for last entry first
+    x[n-1] = round(f[n-1]/A[n-1,n-1],noofdecimal)    # Solve for last entry first
     
     for i in range(n-2,-1,-1):      # Loop from the end to the beginning
         sum_ = 0
         for j in range(i+1,n):         
-            sum_ = sum_ + A[i,j]*x[j]
-        x[i] = round((f[i] - sum_)/A[i,i],3) 
-    print(x) 
+            sum_ = round(sum_ + A[i,j]*x[j],noofdecimal)
+        x[i] = round((f[i] - sum_)/A[i,i],noofdecimal) 
+    return x 
+A = np.array([[1,4,-1],[1,1,-6],[3,-1,-1]]) #has solution [ 1.6479 -1.1408  2.0845]
+f = np.array([-5,-12,4])
+Aug=np.array([[1,4,-1,-5],[1,1,-6,-12],[3,-1,-1,4]])
+numberofunknowns=3 #number of unknowns 
 
-A = np.array([[10,-7,0],[-3,2.099,6],[5,-1,5]])    #has solution[0,-1,1]
-f = np.array([7,3.901,6])
-Aug=np.array([[10,-7,0,7],[-3,2.099,6,3.901],[5,-1,5,6]])
-numberofunknowns=3 #number of unknowns
-gauss_pivot(A,f,Aug,numberofunknowns)              
+noofdecimal=3
+
+answer = gauss_pivot(A,f,Aug,numberofunknowns,noofdecimal)  
+print(answer)
