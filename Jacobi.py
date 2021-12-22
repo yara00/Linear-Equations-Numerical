a37ffbf82ec72aed1caa8e9ec98fcb6abf0349d7
MariamOsman21 <76537563+MariamOsman21@users.noreply.github.com>
2021-12-22 22:49:36 +0200

    Update Jacobi.py

--- Jacobi.py
+++ Jacobi.py
@@ -2,6 +2,17 @@ import numpy
 import numpy as np
 from numpy import linalg as LA
 def jacobi(x, A, b ,tol ,maxIteraions, precision):
+  """
+    function for approximating the solution of system of linear equations using Jacobi iterative method.
+    :param x: array or list representingintial guess.
+    :param A: array or list coeffecient.
+    :param b: array or list b values.
+    :param tol: tolerance float number
+    :param maxIteration: integer representing maximun number of iterations 
+    :param prescion: integer representing number of decimal digits reqired
+    :return: array list representing approximated solution for the equations
+           : integer 0 for no error, 1 if error found
+  """ 
   for k in range(0, maxIteraions):
     x_new = np.empty(len(A))
     for i in range(0, len(A)):
@@ -14,7 +25,7 @@ def jacobi(x, A, b ,tol ,maxIteraions, precision):
       if A[i][i] == 0:
         return x, 1
       x_new[i] = round(sum/round(A[i][i], precision), precision)
-    #Ea calculations
+    #check if tolerance was achieved.
     ea = normCalc(x_new, x)
     if(ea < tol):
       return x_new, 0
@@ -22,10 +33,17 @@ def jacobi(x, A, b ,tol ,maxIteraions, precision):
     x = x_new
   return x, 0
 
-
       
 def normCalc(xnew, xold):
+  """
+    :function for calculating relative error using norm.
+    :param xnew: array or list of the new X values.
+    :param xold: array or list of the old X values.
+    :return: relative error Ea.
+  """ 
+  #suntract new x values from old ones
   ea = np.subtract(xnew,xold)
+  # divide by x new values.
   for i in range(len(xnew)):
     ea[i] = ea[i]/xnew[i]
   return LA.norm(ea)
